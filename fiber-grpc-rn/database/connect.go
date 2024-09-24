c26d7aff51192115ede014234437d511f0371374
Rain <rain@hanbiro.vn>
2024-09-24 09:44:04 +0700

    apply gorm config: prefix

--- fiber-grpc-rn/database/connect.go
+++ fiber-grpc-rn/database/connect.go
@@ -9,6 +9,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 // ConnectDB connect to db
@@ -22,7 +23,12 @@ func ConnectDB() {
 	}
 
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "rn_",
+			SingularTable: true,
+		},
+	})
 
 	if err != nil {
 		panic("failed to connect database")
